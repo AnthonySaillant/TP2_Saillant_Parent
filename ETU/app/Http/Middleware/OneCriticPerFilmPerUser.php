d947772121800957fbb6ab0095cd179f9f3bd1e8
AnthonySaillant <2354779@csfoy.ca>
2025-04-11 09:32:56 -0400

    Creation du middleware critic

---
+++ ETU/app/Http/Middleware/OneCriticPerFilmPerUser.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class OneCriticPerFilmPerUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = auth()->user();
+        $filmId = $request->input('film_id');
+
+        $exists = Critique::where('user_id', $user->id)
+                          ->where('film_id', $filmId)
+                          ->exists();
+
+        if ($exists) {
+            abort(403, 'Vous avez déjà critiqué ce film.');
+        }
+        
+        return $next($request);
+    }
+}
