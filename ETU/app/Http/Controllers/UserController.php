efff34d9038c540b8e4b70bc6b80b0b6e15acfd9
AnthonySaillant <2354779@csfoy.ca>
2025-04-16 09:50:15 -0400

    password validation

--- ETU/app/Http/Controllers/UserController.php
+++ ETU/app/Http/Controllers/UserController.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use App\Http\Resources\UserResource;
 use App\Repository\UserRepositoryInterface;
 
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {   
@@ -33,23 +35,31 @@ class UserController extends Controller
         }     
     }
 
-    public function update(int $id, Request $request){
-        try
-        {
+    public function update(int $id, Request $request)
+    {
+        try {
             $user = User::findOrFail($id);
     
             $validated = $request->validate([
-                'password' => 'required|string|max:255',
+                'current_password' => ['required'],
+                'new_password' => ['required', 'string', 'confirmed', 'max:255',
+                ],
             ]);
     
+            if (!Hash::check($validated['current_password'], $user->password)) {
+                return response()->json([
+                    'message' => 'Mot de passe actuel incorrect.'
+                ], 403);
+            }
+    
+            $validated['password'] = bcrypt($validated['new_password']);
+    
             return (new UserResource($this->userRepository->update($id, $validated)))
-            ->response()
-            ->setStatusCode(OK);
-        }
-
-        catch(Exception $ex)
-        {
-            abort(SERVOR_ERROR, 'server error');
+                ->response()
+                ->setStatusCode(OK);
+    
+        } catch (Exception $ex) {
+            abort(SERVER_ERROR, 'server error');
         }
     }
 }
